a73efa925a80569a944269a9d4d66afb84c15ad6
yilcai1 <caiyiling2009@icloud.com>
2025-05-05 13:49:14 +0800

    Create image_model.py

---
+++ utils/image_model.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class DeepfakeDetectionModel(nn.Module):
+    def __init__(self, num_classes=2):
+        super(DeepfakeDetectionModel, self).__init__()
+        self.efficientnet = models.efficientnet_b0(pretrained=False)
+        num_features = self.efficientnet.classifier[1].in_features
+        self.efficientnet.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(num_features, num_classes),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.efficientnet(x)
